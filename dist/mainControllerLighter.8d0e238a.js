6e1c814babb3672c1d11acdf4754526ca774d386
Jiyeon_Sophia_Lee <jiyeon.sophia.lee@gmail.com>
2021-02-11 18:54:40 -0800

    addeventListener -> onclick in skill.js
    addeventListener -> onclick in skill.js

--- dist/mainControllerLighter.8d0e238a.js
+++ dist/mainControllerLighter.8d0e238a.js
@@ -9199,7 +9199,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14091" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1316" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
