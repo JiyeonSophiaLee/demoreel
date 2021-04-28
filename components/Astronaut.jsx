49ed2b4e378c464c813327a5ebe2280d5318f8a5
Jiyeon_Sophia_Lee <jiyeon.sophia.lee@gmail.com>
2021-04-28 11:14:21 -0700

    working on a directionalLight targeting
    working on a directionalLight targeting

--- components/Astronaut.jsx
+++ components/Astronaut.jsx
@@ -30,7 +30,7 @@ function Model(props) {
   })
   useFrame((state, delta)=>{
     mixer.current.update(delta);
-    state.camera.lookAt(0,1,0)
+    // state.camera.lookAt( 0,1,0)
   })
 
   return  (
@@ -42,14 +42,33 @@ function Model(props) {
 
 
 function Astronaut(props){
+  const [pixelRatio, setPixelRatio] = useState(1);
+  const dirLitRef = useRef();
+
+  useEffect(()=>{ 
+    setPixelRatio(window.devicePixelRatio)
+    dirLitRef.current.target = [-3,0,0];
+  },[])
 
   return(
     <div id='canvas-container'>
-      <Canvas camera={{position:[0,0,5]}}>
+      <Canvas 
+        colorManagement
+        camera={{position:[0,0,5]}} 
+        gl={{ antialias: false, powerPreference:"high-performance" }} 
+        dpr = {pixelRatio}
+        onCreated={canvasCtx => {canvasCtx.gl.physicallyCorrectLights = true}}
+        flat = {THREE.ACESFilmicToneMapping}
+        linear = {THREE.sRGBEncoding}
+        >
         <Suspense fallback={null}>
-          {/* <PerspectiveCamera position={[0,0,15]}  /> */}
-          <pointLight intensity={1} color="rgb(220,51,35)" position={[-1,3,2]}/>
-          <directionalLight intensity={1} color="rgb(43,174,212)" position={[1.5,4,-2]}/>
+          <PerspectiveCamera position={[0,0,5]} makeDefault lookAt={new THREE.Vector3(0, 1, 0)}/>
+          <pointLight intensity={1} color="rgb(220,51,35)" position={[-1,3,2]} />
+          <directionalLight ref={dirLitRef} intensity={10} color="rgb(43,174,212)" position={[1.5,4,-2]} />
+          <Sky size={16} path={'./assets/images/hdr/space_00.jpg'}/>
+          <Sky size={16} path={'./assets/images/hdr/space_01.jpg'}/>
+          <Sky size={16} path={'./assets/images/hdr/space_02.jpg'}/>
+          <Sky size={16} path={'./assets/images/hdr/space_03.jpg'}/>
           <Model vals = {props.vals}/>
         </Suspense>
       </Canvas>
@@ -57,13 +76,13 @@ function Astronaut(props){
   )
 }
 
-// function getSphereGeometry(size){
-//   return(
-//     <mesh>
-//       <sphereGeometry/>
-//       <meshBasicMaterial args={size,25,25}/>
-//     </mesh>
-//   )
-// }
+function Sky(props){
+  return(
+    <mesh>
+      <sphereGeometry attach="geometry" args={[props.size,25,25]}/>
+      <meshBasicMaterial attach="material" map={ new THREE.TextureLoader().load(props.path)} side={THREE.BackSide}/>
+    </mesh>
+  )
+}
 
 export default Astronaut
