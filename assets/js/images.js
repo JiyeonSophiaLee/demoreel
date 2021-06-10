84d5dfdc61391f9e4293936e2cb784f263d9e1da
JiyeonSophiaLee <56799229+JiyeonSophiaLee@users.noreply.github.com>
2021-06-10 20:27:43 +0000

    Deploying to gh-pages from @ JiyeonSophiaLee/demoreel@0fdc14b26a212a21c6a5cd1e53762638803c0cdc 🚀

--- assets/js/images.js
+++ assets/js/images.js
@@ -2,7 +2,7 @@
 //------------SetProperty------------//
 
 
-function SetProperty(src,fileName,files,main,size,alt,skills,artstation=null){
+function SetProperty(src,fileName,files,main,size,alt,skills=null,artstation=null,description=null){
   this.src = src;
   this.fileName = fileName;
   this.files = files;
@@ -11,6 +11,7 @@ function SetProperty(src,fileName,files,main,size,alt,skills,artstation=null){
   this.alt = alt;
   this.skills = skills;
   this.artstation = artstation;
+  this.description = description;
 }
 //------------videos------------//
 
@@ -18,12 +19,12 @@ const astronautSrc = <iframe width="560" height="315" src="https://www.youtube.c
 
 //------------work thumbnails------------//
 
-const safer            = new SetProperty("/assets/images/projects/safer/"           , "safer_"           , ["01.jpg","02.jpg"]                                                       , 0 , {width:1920, height:1080}, "Freelance work for 3D safe"                                           , ['Maya','Arnold','Zbrush','Nuke'] );
+const safer            = new SetProperty("/assets/images/projects/safer/"           , "safer_"           , ["01.jpg","02.jpg"]                                                       , 0 , {width:1920, height:1080}, "Freelance work for 3D safe"                                           , ['Maya','Arnold','Zbrush','Nuke']                                                                                                                                  , null                                      , saferDescrip());
 const astronautRigging = new SetProperty(astronautSrc                               , null               ,  null                                                                     , 0 , null                     , null                                                                   , ['Maya','Blender','GLTF','ThreeJS','Javascript']);
-const table            = new SetProperty("/assets/images/projects/table_scene/"     , "table_scene_"     , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg"]          , 0 , {width:1920, height:1080}, "3D rendering of still lifes for personal project"                     , ['Maya','Arnold','Zbrush','Houdini','Substance Painder','Marvelous Designer','Substance Designer','Mudbox','Photoshop','3D Coat','Rezom UV','After Effects','Nuke'], 'https://www.artstation.com/artwork/Q2roZ');
-const chandelier       = new SetProperty("/assets/images/projects/chandelier_scene/", "chandelier_scene_", ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg"]          , 0 , {width:1920, height:1080}, "3D rendering of chandelier for personal project"                      , ['Maya','Vray','Arnold','Zbrush','Substance Painder','Photoshop','3D Coat','Rezom UV','After Effects','Nuke']                                                    , 'https://www.artstation.com/artwork/Q2roZ');
-const sunrise          = new SetProperty("/assets/images/projects/sunrise_scene/"   , "sunrise_scene_"   , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"]                            , 1 , {width:1920, height:1080}, "3D rendering of carpet with the sun from window for personal project" , ['Maya','Arnold','Zbrush','Substance Painder','Photoshop','3D Coat','Rezom UV','After Effects','Nuke']                                                           , 'https://www.artstation.com/artwork/lVYmZO');
-const woman            = new SetProperty("/assets/images/projects/woman_scene/"     , "woman_scene_"     , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg"] , 2 , {width:1920, height:1080}, "3D rendering of a woman for personal project"                         , ['Maya','Arnold','Zbrush','Houdini','Substance Painder','Mary','Marvelous Designer','Photoshop','3D Coat','Rezom UV','After Effects','Nuke']                      , 'https://www.artstation.com/artwork/xzJvV1');
+const table            = new SetProperty("/assets/images/projects/table_scene/"     , "table_scene_"     , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg"]          , 0 , {width:1920, height:1080}, "3D rendering of still lifes for personal project"                     , ['Maya','Arnold','Zbrush','Houdini','Substance Painder','Marvelous Designer','Substance Designer','Mudbox','Photoshop','3D Coat','Rezom UV','After Effects','Nuke'], 'https://www.artstation.com/artwork/Q2roZ'  );
+const chandelier       = new SetProperty("/assets/images/projects/chandelier_scene/", "chandelier_scene_", ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg"]          , 0 , {width:1920, height:1080}, "3D rendering of chandelier for personal project"                      , ['Maya','Vray','Arnold','Zbrush','Substance Painder','Photoshop','3D Coat','Rezom UV','After Effects','Nuke']                                                      , 'https://www.artstation.com/artwork/Q2roZ', chandelierDescrip());
+const sunrise          = new SetProperty("/assets/images/projects/sunrise_scene/"   , "sunrise_scene_"   , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"]                            , 1 , {width:1920, height:1080}, "3D rendering of carpet with the sun from window for personal project" , ['Maya','Arnold','Zbrush','Substance Painder','Photoshop','3D Coat','Rezom UV','After Effects','Nuke']                                                             , 'https://www.artstation.com/artwork/lVYmZO');
+const woman            = new SetProperty("/assets/images/projects/woman_scene/"     , "woman_scene_"     , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg"] , 2 , {width:1920, height:1080}, "3D rendering of a woman for personal project"                         , ['Maya','Arnold','Zbrush','Houdini','Substance Painder','Mary','Marvelous Designer','Photoshop','3D Coat','Rezom UV','After Effects','Nuke']                       , 'https://www.artstation.com/artwork/xzJvV1');
 const wishingBox       = new SetProperty("/assets/images/projects/wishingBox/"      , "wishingBox_"      , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg"]                   , 2 , {width:1920, height:1080}, "3D animation called WishtingBox"                                      , ['Maya','Vray','Zbrush','Houdini','Substance Painder','Photoshop','3D Coat','Nuke']);
 const giant            = new SetProperty("/assets/images/projects/giant/"           , "giant_"           , ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg"]          , 0 , {width:1920, height:1080}, "3D animation for Giant team"                                          , ['Maya','Vray','Zbrush','Houdini','Substance Painder','Photoshop','3D Coat','Nuke']);
 
@@ -42,3 +43,11 @@ export const paintImages = [paint01, paint02, paint03, paint04, paint05, paint06
 
 
 
+function saferDescrip(){
+  return `This is 3~5 day’s work without rendering time.
+  I did all the tastes from design, modeling, rigging, to post production.`
+}
+function chandelierDescrip(){
+  return `I rendered Arnold and Vray for the same scene to see different refractions and reflections, then combined both rendering images to make the scene rich.
+  The first and second shots are rendered in only one frame and use a 3D camera in Nuke to animate it.`
+}
